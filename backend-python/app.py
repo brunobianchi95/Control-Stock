156050f4d2de455601cb225cef968d4987919f15
FREBAG <francobailon@gmail.com>
2021-12-27 16:55:15 -0300

    creacion SQL

--- backend-python/app.py
+++ backend-python/app.py
@@ -7,15 +7,79 @@ def hello_world():
     return "<p>Que onda rey?</p>"
 
 ############CREACION DEL DB############
-import mysql
+import mysql.connector
+from mysql.connector import errorcode
 
-mydb = mysql.connector.connect(
-  host="127.0.0.1:5000",
-  user="Franco",
+cnx = mysql.connector.connect(
+  host="Localhost",
+  user="root",
   password="catalina"
 )
+cursor = cnx.cursor()
 
-mycursor = mydb.cursor()
+#creacion tablas#
+DB_NAME = 'stock_DB'
 
-mycursor.execute("CREATE DATABASE stock_DB")
+TABLES = {}
+TABLES['users'] = (
+    "CREATE TABLE `users` ("
+    "  `ID` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `first_name` varchar(14) NOT NULL,"
+    "  `last_name` varchar(16) NOT NULL,"
+    "  `admin` BOOLEAN NOT NULL,"
+    "  PRIMARY KEY (`ID`)"
+    ") ENGINE=InnoDB")
 
+TABLES['stock'] = (
+    "CREATE TABLE `stock` ("
+    "  `COD` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `Bebida` varchar(14) NOT NULL,"
+    "  `Brand` varchar(16) NOT NULL,"
+    "  `Cant` INT NOT NULL,"
+    "  PRIMARY KEY (`COD`)"
+    ") ENGINE=InnoDB")
+
+TABLES['ventas'] = (
+    "CREATE TABLE `ventas` ("
+    "  `NO_V` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `ID` int(11) NOT NULL,"
+    "  `COD` int(11) NOT NULL,"
+    "  `Cant` INT NOT NULL,"
+    "  PRIMARY KEY (`NO_V`), KEY `ID` (`ID`), KEY `COD` (`COD`)"
+    ") ENGINE=InnoDB")
+
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("Database {} created successfully.".format(DB_NAME))
+        cnx.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+
+for table_name in TABLES:
+    table_description = TABLES[table_name]
+    try:
+        print("Creating table {}: ".format(table_name), end='')
+        cursor.execute(table_description)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+
+cursor.close()
+cnx.close()
